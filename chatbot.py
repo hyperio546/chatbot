5e574a73a26f259bbcc7bddf8141bf84a1d06e85
flashtechstudios <flashtechstudios@github.com>
2022-06-12 20:20:19 +0530

    change

---
+++ chatbot.py
@@ -0,0 +1,46 @@
+#Credits to my friend for making this file!
+print ("Welcome to chatbot")
+name = input("What is your name? ")
+print ("Hello "+name)
+age = input("How old are you? ")
+color = input("What is your favourite color? ")
+print (color+" is my favourite color too! ")
+hobby = input("What is your hobby? ")
+print ("I really like doing "+hobby+ " too! ")
+input("In which country do you live? ")
+vacation = input("Where would you like to go for vacation? ")
+print ("I would also like to visit "+vacation)
+thing = input("What is the first thing you would do when you go on vacation? ")
+print ("I would also like to "+thing+ " when I go on vacation ")
+car = input("Which car do you/your parents own? ")
+dream = input("What is your dream car? ")
+print ("I would also like to own a "+dream)
+house = input("What is your dream house? ")
+print ("I would also like to live in "+house)
+country = input("Which is your favourite country? ")
+print ("You surely live in a great country ")
+friend = input("Who is your best friend? ")
+print ("I have a good feeling about "+friend)
+confirm = input("Would you be my friend? ")
+if confirm == "no":
+    print ("Oh :c")
+if confirm == "No":
+    print ("Oh :c")
+if confirm == "never":
+    print ("Oh :c")
+if confirm == "Never":
+    print ("Oh :c")
+if confirm == "yes":
+    print ("Yay c:!")
+if confirm == "Yes":
+    print ("Yay c:!")
+if confirm == "yeah":
+    print ("Yay c:!")
+if confirm == "Yeah":
+    print ("Yay c:!")
+if confirm == "yep":
+    print ("Yay c:!")
+if confirm == "Yep":
+    print ("Yay c:!")
+print ("Ok, I will see you soon!")
+print ("Goodbye!")
